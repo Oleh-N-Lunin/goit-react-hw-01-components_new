d2adf373febeb77e420a6214c756b0107cce49d3
Oleh-N-Lunin <67375261+Oleh-N-Lunin@users.noreply.github.com>
2021-07-14 16:39:42 +0300

    1

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,10 +4,11 @@ import PropTypes from "prop-types";
 import StatisticsItems from "./StatisticsItems";
 import styles from './Statistics.module.css';
 
-export default function Statistics({ stats }) {
+
+function Statistics({ stats, title }) {
     return (
         <section className={styles.statistics}>
-            <h2 className={styles.title} >Upload stats</h2>
+            {title && <h2 className={styles.title}>{ title }</h2>}
             <ul className={styles.statList}>
                 {stats.map(item => (
                     <StatisticsItems
@@ -20,6 +21,8 @@ export default function Statistics({ stats }) {
         </section>
     )
 }
+
+export default Statistics;
 
 Statistics.propTypes = {
     items: PropTypes.arrayOf(PropTypes.shape({
